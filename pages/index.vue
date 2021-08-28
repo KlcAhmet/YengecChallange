3abe3938ea65b1eae22a1079d0ad598ac73a6344
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2021-08-29 02:41:10 +0300

    Start project

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Cats/>
+</template>
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Cats from "~/pages/cat image/Cats"
+export default {
+  components:{
+    Cats,
+  }
+}
+</script>
---
+++ pages/mainpage/Main.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="col-2">
+    <img src="https://www.yengecyazilim.com/images/yengecyazilimlogo.png" alt="yangec.png">
+    <h1>Yengeç Yazılım Challange</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
